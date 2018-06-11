888267088ce4e316262252d95bdee3f7db62591d
Nizal Alshammry (PGR) <b0910147@14compsci153.ncl.ac.uk>
2018-06-11 20:17:21 +0100

    delete old patterns

--- src/hyper_go/go.clj
+++ src/hyper_go/go.clj
@@ -52,59 +52,9 @@
 
 (defentity deftransport "" 'transport)
 
-(defclass ToTransport
-  :super
-  (owl-some transports ch/chemical_entity)
-  (owl-some transports-from Location)
-  (owl-some transports-to Location))
-
-(defclass ToTransportTransmembrane
-  :equivalent
-  (owl-and ToTransport
-           (owl-some transports-across Membrane)))
-
-
-;; Pattern 1: transport of some chemical
-;; need to macro this
-(defn transporter
-  ([chemical]
-   ;;{:pre (comment chemical is in ChEBI)}
-   (transporter chemical ToTransport))
-  ([chemical across]
-   (owl-class (str "to-transport " chemical)
-              :equivalent
-              (owl-and across
-                       (owl-some transports chemical)))))
-
-;; Patttern 2: transport of some chemical role
-(defn transporter-with-role
-  ([chemical-role]
-   (transporter chemical-role ToTransport))
-  ([chemical-role across]
-   (transporter
-    (owl-and ch/chemical_entity
-             (owl-some bearer-of chemical-role))
-    across)))
-
-(defn transporter-with-condition [chemical condition]
-  (owl-class (str "to-transport " chemical " with " condition)
-             :equivalent
-             (owl-and (transporter chemical)
-                      (owl-some dependent-on condition))))
-
-(defn transporter-driven [chemical driver]
-  (owl-class (str "to-transport " chemical " driven by " driver)
-             :equivalent
-             (owl-and (transporter chemical)
-                      (owl-some driven-by driver))))
-
-
 (deftransport ToTransportIon
   ;;"GO:0015075"
   :cargo ch/ion)
-
-(def ToTransportIon
-  (transporter ch/ion))
 
 (deftransport ToTransportCation
   ;;"GO:0008324"
@@ -135,11 +85,11 @@
 
 (deftransport ToTransportDrugTransmembrane
   ;;"GO:0015238"
-  :role ch/drug ToTransportTransmembrane)
+  :role ch/drug)
 
 (deftransport ToTransportBiotinTransmembrane
   ;;"GO:0015225"
-  :role ch/biotin ToTransportTransmembrane)
+  :role ch/biotin)
 
 (deftransport ToTransportBicozamycin
   ;;"GO:0015545"
@@ -154,9 +104,9 @@
 
 (defn di-porter [from to]
   (let [first-transport
-        (transporter from)
+        (:cargo :from)
         second-transport
-        (transporter to)]
+        (:carg :to)]
     (owl-class (str "di-porter" from " " to)
                :equivalent
                (owl-and
