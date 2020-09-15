3c7e7b343597f1510415f4c5570a709d038ba792
b0910147 <b0910147@cypher213.ncl.ac.uk>
2020-09-15 12:07:52 +0100

    Fix project version and url

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject hyper-go "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject hyper-go "1.0.0-SNAPSHOT"
+  :description "A Ph.d project to hypernormalise the Gene Ontology (GO)"
+  :url "https://github.com/phillord/hyper-go"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [uk.org.russet/tawny-owl "2.0.1-SNAPSHOT"]
                  [tawny-chebi "1.0.0-SNAPSHOT"]
